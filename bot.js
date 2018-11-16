f060a13d6b63c7d34f42548fc6d19a8aed5116ce
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-17 02:26:34 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
  console.log("welcome bot"); 
-  client.user.setGame(`Blood,ForEver`,"http://twitch.tv/nackzos")
+  client.user.setGame(`.#BloodForEver.`,"http://twitch.tv/nackzosyt")
 console.log("log");
 });
 
 
-client.on('message',function(message) {
-    let prefix = "!SB";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); 
-}
-});
 
 client.login(process.env.BOT_TOKEN); 

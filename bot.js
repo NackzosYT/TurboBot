29db80c2b9ee04e9844b3203685ffa95d200d21b
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-18 20:32:29 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const convert = require("hh-mm-ss")
 const dateFormat = require('dateformat');
@@ -18,65 +18,24 @@ const prefix = 'B'
 
 
 
-//////////////////
+client.on('guildMemberAdd', member => {
+     const welcomer =  member.guild.channels.find('name', 'blood');
 
-const sWlc = {}
-client.on('message', message => {
-if(message.channel.type === "dm") return;
-if(message.author.bot) return;
-  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "blood"
-}
-const channel = sWlc[message.guild.id].channel
-  if (message.content.startsWith(prefix + "setwelcomer")) {
-    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
-    let newChannel = message.content.split(' ').slice(1).join(" ")
-    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
-    sWlc[message.guild.id].channel = newChannel
-    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
-  }
-});
-client.on("guildMemberAdd", member => {
-      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
-    channel: "blood"
-  }
-  const channel = sWlc[member.guild.id].channel
-    const sChannel = sWlc[member.guild.id].channel
-    let welcomer = member.guild.channels.find('name', sChannel);
-    let memberavatar = member.user.avatarURL
-      if (!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var h = mecmber.user;
-        let heroo = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(h.avatarURL)
-        .setAuthor(h.username,h.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)
-         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:heroo});
+const w = ['w1.png'];
+         let Image = Canvas.Image,
+            canvas = new Canvas(400, 239),
+            ctx = canvas.getContext('2d');
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 400, 239);
+             
+          
 
-      var Canvas = require('canvas')
-      var jimp = require('jimp')
-
-      const w = ['w1.png'];
-
-              let Image = Canvas.Image,
-                  canvas = new Canvas(400, 239),
-                  ctx = canvas.getContext('2d');
-
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 400, 239);
-
-      })
-
-                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-               jimp.read(url, (err, ava) => {
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
@@ -92,21 +51,16 @@ client.on("guildMemberAdd", member => {
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
                         ctx.fillText(member.user.username, 240, 210);
-
-                              //AVATARً
  
                 let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
                               ctx.arc(84, 149, 72, 0, Math.PI*2);
-                   ctx.closePath();
-
+                              ctx.stroke();
                                  ctx.clip();
-
                                  ctx.drawImage(ava, 12, 77, 149, 149);
-                              ctx.closePath();
-                         
+                          
                 
                              
 welcomer.sendFile(canvas.toBuffer())
@@ -116,9 +70,10 @@ welcomer.sendFile(canvas.toBuffer())
       
                     
 
+})
+      });     
 
-
-    let channel = member.guild.channels.get('513704573899243520');
+    let channel = member.guild.channels.get('513053776299556894');
     if (!channel) {
         console.log("!channel fails");
         return;
@@ -131,10 +86,9 @@ welcomer.sendFile(canvas.toBuffer())
     while (!guild)
         guild = client.guilds.get("491686948130521112")
 channel.send(`**.#WeeeelcomeTo___Blood___.** :wine_glass:`)   
-
+        
 
 });
-
 
 var dat = JSON.parse("{}");
 client.on("guildMemberAdd", (member) => {
@@ -157,16 +111,12 @@ client.on("guildMemberAdd", (member) => {
                 if (dat[Inv] < Invite.uses) {
                     console.log(3);
                     console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` __***InvitedBy***__: ${Invite.inviter} ♥** `)             }
-
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥** `)            
+ }
             dat[Inv] = Invite.uses;
         })
     })
 });
-
-
-////////////////
-
 
 
 

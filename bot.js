42c84984e43a01462085371f0b1f8e10d8b158e4
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-12-08 18:59:59 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,8 +60,8 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('Bownerbc')){
-if (message.author.id !== '510758300313452545','296047056873848832','212250411351015425') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
-if(!message.author.id !== '510758300313452545','296047056873848832','212250411351015425') return;
+if (message.author.id !== '510758300313452545','510758300313452545','510758300313452545') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+if(!message.author.id !== '510758300313452545','510758300313452545','510758300313452545') return;
 message.channel.sendMessage('جار ارسال |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
